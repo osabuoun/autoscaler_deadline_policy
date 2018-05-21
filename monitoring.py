b464cce9cdf2d8a4129dc0b69a6c9a421f12cfb7
Osama <oabuoun1>
2018-05-21 15:30:34 +0100

    first commit

---
+++ monitoring.py
@@ -0,0 +1,137 @@
+from prometheus_client import Counter, Gauge, Histogram
+import time, sys
+from parameters import statsd
+
+
+JQUEUER_JOB_ADDED 			= 'jqueuer_job_added'
+JQUEUER_JOB_ADDED_TIMESTAMP = 'jqueuer_job_added_timestamp'
+def add_job(experiment_id ,service_name, job_id):
+	statsd.gauge(JQUEUER_JOB_ADDED_TIMESTAMP,
+		time.time(),
+		tags=[
+			'experiment_id:%s' % experiment_id,
+			'service_name:%s' % service_name,
+			'job_id: %s' % job_id,
+		]
+	)
+	statsd.gauge(JQUEUER_JOB_ADDED,
+		time.time(),
+		tags=[
+			'experiment_id:%s' % experiment_id,
+			'service_name:%s' % service_name,
+			'job_id: %s' % job_id,
+		]
+	)
+
+JQUEUER_TASK_ADDED 			= 'jqueuer_task_added'
+JQUEUER_JOB_ADDED_TIMESTAMP = 'jqueuer_task_added_timestamp'
+def add_task(experiment_id ,service_name, job_id, task_id):
+	statsd.gauge(JQUEUER_JOB_ADDED_TIMESTAMP,
+		time.time(),
+		tags=[
+			'experiment_id:%s' % experiment_id,
+			'service_name:%s' % service_name,
+			'job_id: %s' % job_id,
+			'task_id: %s' % task_id,
+		]
+	)
+	statsd.gauge(JQUEUER_TASK_ADDED,
+		time.time(),
+		tags=[
+			'experiment_id:%s' % experiment_id,
+			'service_name:%s' % service_name,
+			'job_id: %s' % job_id,
+			'task_id: %s' % task_id,
+		]
+	)
+
+
+JQUEUER_SERVICE_REPLICAS_RUNNING = 'jqueuer_service_replicas_running'
+def service_replicas_running(experiment_id ,service_name, service_replicas_running):
+	statsd.gauge(JQUEUER_SERVICE_REPLICAS_RUNNING,
+		service_replicas_running,
+		tags=[
+			'experiment_id:%s' % experiment_id,
+			'service_name:%s' % service_name,
+		]
+	)
+
+JQUEUER_SERVICE_REPLICAS_NEEDED = 'jqueuer_service_replicas_needed'
+def service_replicas_needed(experiment_id ,service_name, service_replicas_needed):
+	statsd.gauge(JQUEUER_SERVICE_REPLICAS_NEEDED,
+		service_replicas_needed,
+		tags=[
+			'experiment_id:%s' % experiment_id,
+			'service_name:%s' % service_name,
+		]
+	)
+
+JQUEUER_SERVICE_REPLICAS_MIN = 'jqueuer_service_replicas_min'
+def service_replicas_min(experiment_id ,service_name, service_replicas_min):
+	statsd.gauge(JQUEUER_SERVICE_REPLICAS_MIN,
+		service_replicas_min,
+		tags=[
+			'experiment_id:%s' % experiment_id,
+			'service_name:%s' % service_name,
+		]
+	)
+
+JQUEUER_SERVICE_REPLICAS_MAX = 'jqueuer_service_replicas_max'
+def service_replicas_max(experiment_id ,service_name, service_replicas_max):
+	statsd.gauge(JQUEUER_SERVICE_REPLICAS_MAX,
+		service_replicas_max,
+		tags=[
+			'experiment_id:%s' % experiment_id,
+			'service_name:%s' % service_name,
+		]
+	)
+
+JQUEUER_SINGLE_TASK_DURATION = 'jqueuer_single_task_duration'
+def single_task_duration(experiment_id ,service_name, single_task_duration):
+	statsd.gauge(JQUEUER_SINGLE_TASK_DURATION,
+		single_task_duration,
+		tags=[
+			'experiment_id:%s' % experiment_id,
+			'service_name:%s' % service_name,
+		]
+	)
+
+JQUEUER_EXPERIMENT_ACTUAL_START_TIMESTAMP = 'jqueuer_experiment_actual_start_timestamp'
+def experiment_actual_start_timestamp(experiment_id ,service_name, experiment_actual_start_timestamp):
+	statsd.gauge(JQUEUER_EXPERIMENT_ACTUAL_START_TIMESTAMP,
+		experiment_actual_start_timestamp,
+		tags=[
+			'experiment_id:%s' % experiment_id,
+			'service_name:%s' % service_name,
+		]
+	)
+
+JQUEUER_EXPERIMENT_DEADLINE_TIMESTAMP = 'jqueuer_experiment_deadline_timestamp'
+def experiment_deadline_timestamp(experiment_id ,service_name, experiment_deadline_timestamp):
+	statsd.gauge(JQUEUER_EXPERIMENT_DEADLINE_TIMESTAMP,
+		experiment_deadline_timestamp,
+		tags=[
+			'experiment_id:%s' % experiment_id,
+			'service_name:%s' % service_name,
+		]
+	)
+
+JQUEUER_EXPERIMENT_ACTUAL_END_TIMESTAMP = 'jqueuer_experiment_actual_end_timestamp'
+def experiment_actual_end_timestamp(experiment_id ,service_name, experiment_actual_end_timestamp):
+	statsd.gauge(JQUEUER_EXPERIMENT_ACTUAL_END_TIMESTAMP,
+		experiment_actual_end_timestamp,
+		tags=[
+			'experiment_id:%s' % experiment_id,
+			'service_name:%s' % service_name,
+		]
+	)
+
+JQUEUER_EXPERIMENT_RUNNING_TIMESTAMP = 'jqueuer_experiment_running_timestamp'
+def experiment_running_timestamp(experiment_id ,service_name, experiment_running_timestamp):
+	statsd.gauge(JQUEUER_EXPERIMENT_RUNNING_TIMESTAMP,
+		experiment_running_timestamp,
+		tags=[
+			'experiment_id:%s' % experiment_id,
+			'service_name:%s' % service_name,
+		]
+	)
---
+++ parameters.py
@@ -0,0 +1,59 @@
+import redis
+JOB_QUEUE_PREFIX 	= 	'jqueue_service_'
+broker_protocol		= 	'pyamqp'
+#broker_username 	= 	'guest'
+broker_username 	= 	'admin'
+#broker_password 	= 	''
+broker_password 	= 	'mypass'
+#broker_server		=	'192.168.253.1'
+broker_server		=	'rabbit'
+broker_port 		= 	5672
+
+def broker():
+	broker 	= 	broker_protocol + '://' + broker_username 
+	if (broker_password != ''):
+		broker 	= broker + ':' + broker_password
+	broker 	= 	broker + '@' + broker_server + ':' + str(broker_port) + '//'
+	return broker
+
+backend_protocol 			= 	'redis'
+#backend_server	 			=	'192.168.253.1'
+backend_server	 			=	'redis'
+backend_port     			=	6379
+backend_db		 			=	0
+backend_experiment_db_id	=	10
+
+backend_experiment_db = redis.StrictRedis(
+	host=backend_server, 
+	port=backend_port, 
+	db=backend_experiment_db_id, 
+	charset="utf-8", 
+	decode_responses=True
+	)
+ 	
+def backend(db):
+	backend = backend_protocol + '://' + backend_server + ':' + str(backend_port) + '/' + str(db)
+	return backend
+
+
+#from datadog import DogStatsd
+
+#STASTD_SERVER 	= '192.168.253.1'
+STATSD_SERVER 	= 'statsd'
+STATSD_PORT		= 9125
+
+
+#statsd = DogStatsd(host=STATSD_SERVER, port=STATSD_PORT)
+
+STATSD_OPTIONS = {
+    'api_key':'jqueuer_api_key',
+    'app_key':'jqueuer_app_key',
+    'statsd_host': STATSD_SERVER,
+    'statsd_port': STATSD_PORT
+}
+
+from datadog import initialize
+
+initialize(**STATSD_OPTIONS)
+
+from datadog import statsd
